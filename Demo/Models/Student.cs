38650f6d2ac7e4077f0d43917c5a4d9e2c3a3616
mylaptop <hieuvanphan275@gmail.com>
2021-10-14 21:11:07 +0700

    demo

---
+++ Demo/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo.Models
+{
+    public class Student
+    {
+        public string RollNumber  { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set;}
+        public string Phone { get; set; }
+        public DateTime CreateAt { get; set; }
+        public StudentStatus Status { get; set; }
+        public override string ToString()
+        {
+            return $"Rollnumber{RollNumber},Fullname{FullName},Email{Email},Birthday{Birthday},Phone{Phone},Status{Status},Datetime{CreateAt}";
+        }
+
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Demo.Models;
+
+namespace Demo
+{
+    public enum StudentStatus
+    {
+        Active = 1,
+        Deactive = 0,
+        Deleted = -1
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var st = new Student
+            {
+                RollNumber = "H1",
+                FullName = "Phan Hieu",
+                Birthday = DateTime.Parse("2002-03-13"),
+                Email = "hieu@gmail.com",
+                Phone = "0987654231",
+                CreateAt = DateTime.Now,
+                Status = StudentStatus.Active
+            };
+            //Console.WriteLine(st.ToString());
+            Console.WriteLine((int)st.Status);
+        }
+    }
+}
